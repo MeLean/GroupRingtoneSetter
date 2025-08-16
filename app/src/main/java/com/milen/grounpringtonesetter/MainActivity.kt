0b47ce471fb966e1c1c112db6d9446f4c8723280
Milen Dimitrov <milen.dimitrov@paysafe.com>
2025-08-16 10:57:08 +0300

    Version 6.7.7
    Apply edge-to-edge

--- app/src/main/java/com/milen/grounpringtonesetter/MainActivity.kt
+++ app/src/main/java/com/milen/grounpringtonesetter/MainActivity.kt
@@ -4,6 +4,8 @@ package com.milen.grounpringtonesetter
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -11,6 +13,8 @@ import com.google.android.gms.ads.MobileAds
 import com.milen.grounpringtonesetter.databinding.ActivityMainBinding
 import com.milen.grounpringtonesetter.screens.viewmodel.MainViewModel
 import com.milen.grounpringtonesetter.screens.viewmodel.MainViewModelFactory
+import com.milen.grounpringtonesetter.utils.applyNavAndImePadding
+import com.milen.grounpringtonesetter.utils.applyStatusBarPadding
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,8 +28,20 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            isAppearanceLightStatusBars = true
+            isAppearanceLightNavigationBars = true
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toolbarMain.applyStatusBarPadding()
+        binding.container.applyNavAndImePadding()
 
         MobileAds.initialize(this)
 

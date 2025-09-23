72e6c4ba1cb5fadd4a982fbbf834a99572eb1033
Milen Dimitrov <milen.dimitrov@paysafe.com>
2025-09-23 20:11:56 +0300

    Revert "Version 7.0.5"
    This reverts commit 3542a5ebea02d74957fdf7aa11f74e6579594662.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.milen.grounpringtonesetter"
         minSdk = 27
         targetSdk = 35
-        versionCode = 704
-        versionName = "7.0.4"
+        versionCode = 705
+        versionName = "7.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }

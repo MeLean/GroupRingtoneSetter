47fa1ddb273def0c0e6a7121c52a6187dba7d431
Milen Dimitrov <milen.dimitrov@paysafe.com>
2025-10-04 10:45:08 +0300

    Version 7.2.7
    Improve the BillingGuard.kt to avoid NPE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.milen.grounpringtonesetter"
         minSdk = 27
         targetSdk = 35
-        versionCode = 717
-        versionName = "7.1.7"
+        versionCode = 727
+        versionName = "7.2.7"
         
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

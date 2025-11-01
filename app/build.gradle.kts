bc9b89f0bfb154d041eccfac617fcafabf0da472
Milen Dimitrov <milen.dimitrov@paysafe.com>
2025-11-01 11:39:09 +0200

    Version 7.7.8
    Rollback changes but add german
    Bump to 7.7.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.milen.grounpringtonesetter"
         minSdk = 27
         targetSdk = 35
-        versionCode = 737
-        versionName = "7.3.7"
+        versionCode = 778
+        versionName = "7.7.8"
         
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

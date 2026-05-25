2e9b2e836843aa8d17c05fb10b42cff5ada09ea0
Milen Dimitrov <milen.dimitrov@paysafe.com>
2026-05-25 15:07:28 +0300

    Bump version to 8.3.3.
    Key changes:
    - Update `versionCode` to 833 and `versionName` to 8.3.3 in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.milen.grounpringtonesetter"
         minSdk = 27
         targetSdk = 35
-        versionCode = 832
-        versionName = "8.3.2"
+        versionCode = 833
+        versionName = "8.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
